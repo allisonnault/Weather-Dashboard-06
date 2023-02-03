14431e71dbe692c4e04072ab54106c22ba54dfde
allisonnault <allisonrnault@gmail.com>
2023-02-03 14:44:07 -0600

    added basic files and bootstrap

---
+++ psuedo.js
@@ -0,0 +1,6 @@
+// register for open weathermap.org
+    // link in readme for documentation 
+    // will need geocoding api for lat & long
+    // go through all the resources (slack channel)
+
+// 
