ead76310184fd919f4a64da3944860a343d394ac
allisonnault <allisonrnault@gmail.com>
2023-02-03 19:22:01 -0600

    working on the javascript

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,16 @@
 dayjs().format();
+today = dayjs().format('M/D/YYYY');
 var openWeatherKey = "28b9f68edfd5dc9c73e3e4892e6278f5";
 var city;
 var lat;
 var lon;
-
+var currentCity = $('#CityDate');
+var currentTemp = $('#temp');
+var currentWind = $('#wind');
+var currentHumidity = $('#humidity');
 var searchCity = $('input[name="search"]');
 var searchBtn = $('#searchBtn');
+var fiveDay = [];
 
 function openWeatherAPI() {
     var openWeatherURL = "http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + openWeatherKey;
@@ -14,12 +19,24 @@ function openWeatherAPI() {
             return response.json();
         })
         .then(function (data) {
+            for (var i = 0; i < data.length; i +8) {
+                var date = dayjs.unix(data.list[i].dt).format("MM/DD/YY");
+                var temp = Math.trunc((data.list[i].main.temp - 273.15) * (9 / 5) + 32) + "°F";
+                var wind = ((data.list[i].wind.speed) * 2.23694).toFixed(2) + " MPH";
+                var humidity = data.list[i].main.humidity + '%';
 
-            console.log(data.list);
-            var currentTemp = Math.trunc((data.list[0].main.temp - 273.15) * (9 / 5) + 32) + "°F";
-            var currentWind = ((data.list[0].wind.speed) * 2.23694).toFixed(2) + "MPH";
-            var currentHumidity = data.list[0].main.humidity + '%';
+                var fiveDayInfo = {
+                    fiveDate: date,
+                    fiveTemp: temp,
+                    fiveWind: wind,
+                    fiveHumidity: humidity
+                }
 
+              fiveDay.push(fiveDayInfo);
+              console.log(fiveDay);
+
+            }
+            // console.log(data.list);
         }
         );
 }
@@ -33,7 +50,9 @@ function geoCodeAPI() {
         .then(function (data) {
             lat = data[0].lat;
             lon = data[0].lon;
+            currentCity.text(data[0].name + ", " + data[0].state + " - " + today);
             openWeatherAPI();
+            // console.log(data);
         });
 }
 
@@ -43,5 +62,11 @@ searchBtn.on('click', function () {
     } else {
         city = $.trim(searchCity.val());
     }
+
     geoCodeAPI();
 })
+
+
+// currentTemp.text("Temp: " + temp);
+// currentWind.text("Wind: " + wind);
+// currentHumidity.text("Humidity: " + humidity);
