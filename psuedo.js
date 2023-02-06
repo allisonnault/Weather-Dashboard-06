5a75f07a42eec848149f9179315e252089f068e8
allisonnault <allisonrnault@gmail.com>
2023-02-05 18:03:58 -0600

    changed font

--- psuedo.js
+++
@@ -1,6 +0,0 @@
-// register for open weathermap.org
-    // link in readme for documentation 
-    // will need geocoding api for lat & long
-    // go through all the resources (slack channel)
-
-// f
